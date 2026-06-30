1f268fb670758913a4ed334ee77663b825505c28
itzhaolei <itzhaolei@foxmail.com>
2026-06-30 21:36:22 +0800

    Restore login startup flow

--- Morvi/Sources/Application/SceneDelegate.swift
+++ Morvi/Sources/Application/SceneDelegate.swift
@@ -24,6 +24,6 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             }
         }
         #endif
-        return RootTabsController()
+        return EntrySceneController()
     }
 }
