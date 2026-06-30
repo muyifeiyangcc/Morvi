5b2dd4c227a4f9e303ce09d5b4dca83fda63c545
itzhaolei <itzhaolei@foxmail.com>
2026-06-30 15:26:57 +0800

    Use solid decorative background

--- Morvi/Sources/Shared/Views/DecorativeGradientView.swift
+++ Morvi/Sources/Shared/Views/DecorativeGradientView.swift
@@ -4,20 +4,12 @@ final class DecorativeGradientView: UIView {
     enum Palette {
         case blue
 
-        var colors: [CGColor] {
+        var backgroundColor: UIColor {
             switch self {
             case .blue:
-                return [
-                    UIColor(red: 0.78, green: 0.92, blue: 1.00, alpha: 1).cgColor,
-                    UIColor(red: 0.42, green: 0.67, blue: 1.00, alpha: 1).cgColor,
-                    UIColor(red: 0.12, green: 0.32, blue: 0.82, alpha: 1).cgColor
-                ]
+                return UIColor(red: 0.42, green: 0.67, blue: 1.00, alpha: 1)
             }
         }
-    }
-
-    override class var layerClass: AnyClass {
-        CAGradientLayer.self
     }
 
     private let palette: Palette
@@ -26,18 +18,10 @@ final class DecorativeGradientView: UIView {
         self.palette = palette
         super.init(frame: .zero)
         isUserInteractionEnabled = false
-        configureGradient()
+        backgroundColor = palette.backgroundColor
     }
 
     required init?(coder: NSCoder) {
         nil
-    }
-
-    private func configureGradient() {
-        guard let gradientLayer = layer as? CAGradientLayer else { return }
-        gradientLayer.colors = palette.colors
-        gradientLayer.locations = [0, 0.52, 1]
-        gradientLayer.startPoint = CGPoint(x: 0.12, y: 0)
-        gradientLayer.endPoint = CGPoint(x: 0.88, y: 1)
     }
 }
